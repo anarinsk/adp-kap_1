bf6fb03944b3dddfe63081bceaa342d713c193cf
Jun Sok Huhh <anarinsk@gmail.com>
2020-02-05 17:32:29 +0900

    eda-시군구: 90%

--- 시군구/eda.py
+++ 시군구/eda.py
@@ -15,10 +15,11 @@ mpl.style.use('seaborn-pastel')
 plt.rcParams["font.family"] = 'NanumBarunGothic'
 plt.rcParams['axes.unicode_minus'] = False
 plt.rcParams['font.size'] = 10.
-plt.rcParams['xtick.labelsize'] = 9
-plt.rcParams['ytick.labelsize'] = 9
-plt.rcParams['axes.labelsize'] = 15.
+plt.rcParams['xtick.labelsize'] = 9.
+plt.rcParams['ytick.labelsize'] = 9.
+plt.rcParams['axes.labelsize'] = 12.
 
+# %% Misc. & Params 
 def gen_dir(terminal, where=here):
     if where == "work": 
         base_dir = 'D:/'
@@ -29,32 +30,28 @@ def gen_dir(terminal, where=here):
     return os.path.join(base_dir, github_dir, terminal)
 
 here = "home"
-# %%
+# %% Load, Pivot, Massage
 df = pd.read_pickle(gen_dir('시군구/시군구1.pkl'))
-# %%
 df1 = df.stack(level=['광역','시군구1']).reset_index()
 df1.drop(columns = ['index'], inplace=True)
 df1.rename(columns={'level_0': '월'}, inplace=True)
 
-# %%
+# %% Custom filters 
 my_filter1 = (df1['청년 구매율'].notna()) & (df1['청년 구매율'] > 0) & (df1['합계'] > 20) 
 my_filter2 = my_filter1 & (df1['광역']=='서울') 
 df2 = df1.loc[my_filter2]
-df2
+
 # %%
 df_cor = df2.groupby(['시군구1'])[['청년 구매율', '합계']].corr(method='kendall')
 df_cor.reset_index(inplace=True)
-df_cor
 df_cor = df_cor.loc[(df_cor['합계'].notna())]
+df_cor = df_cor[df_cor['level_1'] == "청년 구매율"][['시군구1', '합계']]
+df_cor.columns = ['시군구', '상관계수']
+df_cor.sort_values(['상관계수'], ascending=False)
 #%%
-dft = df_cor[df_cor['level_1'] == "청년 구매율"][['시군구1', '합계']]
-dft.rename(columns={'합계': '상관계수'}, inplace=True)
-dftt = dft.sort_values(['상관계수'], ascending=False)
-#dft.plot.bar(x='광역', y='합계')
-#plt.ylabel(r'$\sin (x)$')
-# %%
-plt.figure(figsize=(10,10))
-ax = dftt.sort_values(['상관계수']).plot.barh(x='시군구1', y='상관계수', rot=0)
+p_df = df_cor.sort_values(['상관계수'])
+fig, ax = plt.subplots(figsize=(9, 7))
+ax.barh(p_df['시군구'], p_df['상관계수'], align='center')
 ax.legend([r'Kendall $\tau$'])
-plt.savefig('kendalltau.png', dpi=300)
+#plt.savefig('kendalltau.png', dpi=300)
 # %%
